48d334ff564f0e26fe00eaabdc021e006cb0b09a
mingyu_choi <ligyu3465@gmail.com>
2021-09-05 01:35:29 +0900

    add html

--- mukbang/urls.py
+++ mukbang/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('list/', views.list, name='list'),
     
-    path('test/', views.test, name='test'),
+  
     path('create/', views.create, name='create'),
+    path('test/', views.test, name='test'),
     path('list_test/', views.list_test, name='list_test'),
 ]
