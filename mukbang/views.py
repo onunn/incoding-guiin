b201238b93445ec2817ed55abcc33beceb73850c
mskim0114 <66244354+mskim0114@users.noreply.github.com>
2021-09-04 21:49:43 +0900

    test1

--- mukbang/views.py
+++ mukbang/views.py
@@ -4,7 +4,15 @@ from django.http import HttpResponse
 from django.template import loader
 from datetime import datetime
 
+from mukbang.models import Muckbang
+from mukbang.forms import Muckbangform
+
 import random
+
+from django.http import HttpResponseRedirect
+from django.db.models import Count, Avg
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
@@ -13,3 +21,16 @@ def index(request):
         'current_date':now
     }
     return render(request, 'first/index.html', context)
+
+def list(request):
+    youtuber = Muckbang.objects.all()
+    paginator = Muckbang(youtuber, 5)
+
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+
+    context = {
+        'youtuber': items
+    }
+
+    return render(request, 'first/list.html ', context)
