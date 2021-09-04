31a6f281412bff969d8c060c01072217b94f1d9a
mskim0114 <66244354+mskim0114@users.noreply.github.com>
2021-09-04 19:00:47 +0900

    test1

--- mukbang/models.py
+++ mukbang/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Muckbang(models.Model):
+    group = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
+    tag = models.CharField(max_length=20)
+    link = models.TextField()
+    description = models.TextField()
+    image = models.ImageField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- mukbang/views.py
+++ mukbang/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.template import loader
+from datetime import datetime
+
+import random
 
 # Create your views here.
+def index(request):
+    now = datetime.now()
+    context = {
+        'current_date':now
+    }
+    return render(request, 'first/index.html', context)
