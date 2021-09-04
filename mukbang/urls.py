483b3a3c3cc4ea5a354da1d733138cfff3f50309
mskim0114 <66244354+mskim0114@users.noreply.github.com>
2021-09-05 01:49:43 +0900

    test9

--- mukbang/urls.py
+++ mukbang/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('list/', views.list, name='list'),
     
     path('test/', views.test, name='test'),
-    path('<int:group_id>/create/', views.create, name='create'),
+    path('<int:youtuber_id>/create/', views.create, name='create'),
     # path('restaurant/<int:restaurant_id>/review/create', views.review_create, name='review-create'),
-    path('<int:group_id>youtuber_list/', views.youtuber_list, name='youtuber_list'),
+    path('<int:youtuber_id>youtuber_list/', views.youtuber_list, name='youtuber_list'),
 
     path('list_test/', views.list_test, name='list_test'),
 ]
