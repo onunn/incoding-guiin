d9dc8fef50d042b79ba1c94d8b9b1dd835132931
mingyu_choi <ligyu3465@gmail.com>
2021-09-05 03:33:20 +0900

    test

--- mukbang/urls.py
+++ mukbang/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('list/', views.list, name='list'),
     
-  
-    path('create/', views.create, name='create'),
     path('test/', views.test, name='test'),
+    path('<int:group_id>/create/', views.create, name='create'),
+    # path('restaurant/<int:restaurant_id>/review/create', views.review_create, name='review-create'),
+    path('<int:group_id>youtuber_list/', views.youtuber_list, name='youtuber_list'),
+
     path('list_test/', views.list_test, name='list_test'),
 ]
