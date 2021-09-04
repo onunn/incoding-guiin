7e93e8ebd2dca8894b225a7b297b9aad19f7736e
hjs9605 <hjs9605@naver.com>
2021-09-05 04:55:05 +0900

    question real done

--- mukbang/urls.py
+++ mukbang/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     # path('restaurant/<int:restaurant_id>/review/create', views.review_create, name='review-create'),
     path('<int:group_id>youtuber_list/', views.youtuber_list, name='youtuber_list'),
-
+    path('question_test/',views.question_test, name="question_test"),
     path('list_test/', views.list_test, name='list_test'),
     path('list_test/1', views.list_test, name='list_test'),
     path('list_test/2', views.list_test, name='list_test'),
